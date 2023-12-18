723cd0f62eb9249eb2bad7b27b9f6a5c1df4d654
pandrosov <pandrosov@list.ru>
2023-12-18 16:03:08 +0300

    [FIX BUGS] add font-size for invoice custom report

--- accounting_move_improve/models/account_move.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from odoo import models, fields, api
-
-
-class AccountMoveImprove(models.Model):
-    _inherit = 'account.move'
-
-    documents = fields.Many2many('ir.attachment', String='Attachments')
